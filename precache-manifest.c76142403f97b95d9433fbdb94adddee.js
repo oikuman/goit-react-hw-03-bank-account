e8aad5a0c7f7b35d092fc9bf699cc0ed320dfb97
oikuman <oikuma@gmail.com>
2019-10-15 01:00:42 +0300

    Updates

---
+++ precache-manifest.c76142403f97b95d9433fbdb94adddee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1a12b1f59b51ab8177c037e952a616",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "27267c6a2fd52238c0d6",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "57927eb79b9e9d8e644f",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.fd873468.chunk.css"
+  },
+  {
+    "revision": "27267c6a2fd52238c0d6",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.10cc287b.chunk.js"
+  },
+  {
+    "revision": "57927eb79b9e9d8e644f",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.5b5004a9.chunk.js"
+  },
+  {
+    "revision": "933e7be94ae0a19ccc21",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.a53e7bae.js"
+  }
+]);
